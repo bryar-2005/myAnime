c2130de6c7dcbfc73692bab37a490372a5e69da0
Bryar <bryarpshtewan27@gmail.com>
2025-12-28 20:50:27 +0300

    Force Laravel to serve React frontend

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
-    return view('welcome');
+    // If Apache passes the request to PHP, serve the React Frontend
+    return File::get(public_path('index.html'));
+});
+
+// Optional: specific fallback for other non-API routes if needed
+Route::fallback(function () {
+    return File::get(public_path('index.html'));
 });
